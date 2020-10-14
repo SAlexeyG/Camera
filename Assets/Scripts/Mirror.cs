c1a81ade3f654f8bf7eae9971f92abb9ff64b329
Alexey Smyshlaev <alexeysm@playtika.com>
2020-10-14 18:38:54 +0300

    render texture

---
+++ Assets/Scripts/Mirror.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var renderTexture = new RenderTexture(512, 512, 16, RenderTextureFormat.ARGB32);
+        camera.targetTexture = renderTexture;
+        GetComponent<MeshRenderer>().material.SetTexture(1, renderTexture, UnityEngine.Rendering.RenderTextureSubElement.Color);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Buttons
+{
+    WASD,
+    Arrows,
+
+}
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float sensitivity;
+    [SerializeField] private Buttons buttons;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = 0f;
+        float verticalAxis = 0f;
+
+        switch(buttons)
+        {
+            case Buttons.WASD:
+                horizontalAxis = Input.GetKey(KeyCode.A) ? -1f : horizontalAxis;
+                horizontalAxis = Input.GetKey(KeyCode.D) ? 1f : horizontalAxis;
+                verticalAxis = Input.GetKey(KeyCode.W) ? 1f : verticalAxis;
+                verticalAxis = Input.GetKey(KeyCode.S) ? -1f : verticalAxis;
+                break;
+
+            case Buttons.Arrows:
+                horizontalAxis = Input.GetKey(KeyCode.LeftArrow) ? -1f : horizontalAxis;
+                horizontalAxis = Input.GetKey(KeyCode.RightArrow) ? 1f : horizontalAxis;
+                verticalAxis = Input.GetKey(KeyCode.UpArrow) ? 1f : verticalAxis;
+                verticalAxis = Input.GetKey(KeyCode.DownArrow) ? -1f : verticalAxis;
+                break;
+        }
+
+        transform.Translate(
+            horizontalAxis * Time.deltaTime * speed,
+            0,
+            verticalAxis * Time.deltaTime * speed);
+
+        transform.Rotate(
+            0f,
+            Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity,
+            0f);
+    }
+}
